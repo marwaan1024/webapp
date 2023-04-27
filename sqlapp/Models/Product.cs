3bc2d2df27e18ebb83729fd725168ac7a5bc3de0
marwa <marwa@MARWAANS-IDEAPA>
2023-04-27 14:34:43 +0530

    Add project files.

---
+++ sqlapp/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace sqlapp.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+
+        public string ProductName { get; set; } 
+
+        public int Quantity { get; set; }
+
+
+    }
+}
